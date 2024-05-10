c99b71fc9ad33d2259f8ed82dc22d7da03d605cd
gamithf <125730055+gamithf@users.noreply.github.com>
2024-05-10 07:24:42 +0530

    Created DAO file for db operations

--- app/src/main/java/com/example/taskmanagementapp/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagementapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.taskmanagementapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.taskmanagementapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addButton.setOnClickListener {
+            val intent = Intent(this, AddTaskActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
