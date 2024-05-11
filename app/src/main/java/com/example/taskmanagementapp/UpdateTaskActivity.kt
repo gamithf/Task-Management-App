123bf8a6f7804414f8203fee40b262950536a4ef
gamithf <125730055+gamithf@users.noreply.github.com>
2024-05-11 07:59:40 +0530

    Completed Update Task operation

---
+++ app/src/main/java/com/example/taskmanagementapp/UpdateTaskActivity.kt
@@ -0,0 +1,42 @@
+package com.example.taskmanagementapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.taskmanagementapp.databinding.ActivityUpdateTaskBinding
+
+class UpdateTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUpdateTaskBinding
+    private lateinit var  db: TaskDAO
+    private var taskID = -1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUpdateTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = TaskDAO(this)
+
+        taskID = intent.getIntExtra("task_id", -1)
+        if(taskID == -1) {
+            finish()
+            return
+        }
+
+        val task = db.getTaskByID(taskID)
+        binding.updateTitleEditText.setText(task.title)
+        binding.updateContentEditText.setText(task.content)
+
+        binding.updateSaveButton.setOnClickListener {
+            val newTitile = binding.updateTitleEditText.text.toString()
+            val newContent = binding.updateContentEditText.text.toString()
+            val updatedTask = Task(taskID, newTitile, newContent)
+            db.updateTask(updatedTask)
+            finish()
+
+            Toast.makeText(this, "Task Updated!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
